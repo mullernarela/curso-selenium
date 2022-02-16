596126d7717f0345817faed9bd9fa5b4cc2970d3
narel <narel@LAPTOP-S8D8K3R8>
2022-02-15 23:33:39 -0300

    Add Chrome driver Add new classes DataTypes Operadores

---
+++ SeleniumCursoEnero/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Datos primitivos
+
+		byte var1 = 1;
+		short var2 = 1;
+		int var3 = 1;
+		long var4 = 1;
+		float var5 = 1;
+		double var6 = 1;
+		char var7 = 1;
+		boolean var8 = true;
+
+		System.out.println(var8);
+
+	}
+
+}
